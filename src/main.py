0100ef54aeefef4c61d3b836588216f78ec948f7
Mario Juez <marioajuez@gmail.com>
2025-06-23 17:33:08 -0500

    other test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+import os 
 from dotenv import load_dotenv
 load_dotenv()
+
+os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "0"
 
 from routes.product_routes import router as product_routes
 from fastapi import FastAPI
