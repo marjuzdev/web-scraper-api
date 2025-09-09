ff331ada468c71dbc467f4a5acb5de59a9ab29ee
Mario Juez <marioajuez@gmail.com>
2025-09-08 20:08:29 -0500

    add crud benie & add work with Native motor Mongo with operatios example

--- src/repositories/test_repository.py
+++ src/repositories/test_repository.py
@@ -1,3 +1,4 @@
+from beanie import PydanticObjectId
 from fastapi import HTTPException
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorDatabase
@@ -9,7 +10,7 @@ logger = configure_logger()
 class TestRepository:
     
     def __init__(self, db: AsyncIOMotorDatabase = None):
-        self.collection = db["marketplaces"] if db else None
+        self.collection = db["test"] if db is not None else None
 
     # -----------------------------
     # CRUD con Beanie
@@ -37,15 +38,83 @@ class TestRepository:
         if not entity:
             raise HTTPException(status_code=404, detail="Entity not found")
         return entity
+    
+    async def get_all(self):
+        try:
+
+            docs = await TestEntity.find().to_list(100)
+            return [
+                {**doc, "_id": str(doc["_id"])} 
+                if "_id" in doc and isinstance(doc["_id"], ObjectId) else doc
+                for doc in docs
+            ]
+        
+        except Exception:
+            logger.exception("Unexpected error while listing marketplaces")
+            raise HTTPException(status_code=500, detail="Failed to list marketplaces")
+        
+    async def delete(self, test_id: str):
+        try:
+            if not PydanticObjectId.is_valid(test_id):
+                raise HTTPException(status_code=400, detail="Invalid ID format")
+
+            test = await TestEntity.get(PydanticObjectId(test_id))
+            if not test:
+                raise HTTPException(status_code=404, detail="Test not found")
+
+            await test.delete()
+            return {"deleted": True}
+        except HTTPException:
+            raise
+        except Exception:
+            logger.exception("Unexpected error while deleting test entity")
+            raise HTTPException(status_code=500, detail="Failed to delete test entity")
+        
+    async def update(self, test_id: str, update_data: dict):
+        try:
+            if not PydanticObjectId.is_valid(test_id):
+                raise HTTPException(status_code=400, detail="Invalid ID format")
+
+            result = await TestEntity.find_one(
+                TestEntity.id == PydanticObjectId(test_id)
+            ).update({"$set": update_data})
+
+            if result is None:
+                raise HTTPException(status_code=404, detail="Test not found")
+
+            return {"updated": True}
+
+        except HTTPException as e:
+            logger.error(f"HTTPException: {e.detail}")
+            raise e
+        except Exception as e:
+            logger.exception(f"Unexpected error while updating test entity: {str(e)}")
+            raise HTTPException(status_code=500, detail="Failed to update test entity")
 
     # -----------------------------
     # Funciones opcionales con Motor
     # -----------------------------
     async def raw_find(self, filter_query: dict, limit: int = 50):
-        if not self.collection:
+        
+        if self.collection is None:
             raise HTTPException(status_code=500, detail="Motor not initialized")
+         
         try:
-            return await self.collection.find(filter_query).to_list(length=limit)
+            filter_mongo = {}
+            if filter_query.get("name") is not None:
+                filter_mongo["name"] = {"$regex": filter_query["name"], "$options": "i"}
+
+            if filter_query.get("country") is not None:
+                filter_mongo["country"] = {"$eq": filter_query["country"]}
+
+            docs = await self.collection.find(filter_mongo).to_list(length=limit)
+
+            return [
+                {**doc, "_id": str(doc["_id"])} 
+                if "_id" in doc and isinstance(doc["_id"], ObjectId) else doc
+                for doc in docs
+            ]
+        
         except Exception as e:
             logger.exception(f"Error in raw find: {e}")
             raise HTTPException(status_code=500, detail="Failed raw query")
--- src/routes/test_routes.py
+++ src/routes/test_routes.py
@@ -1,38 +1,105 @@
 from fastapi import APIRouter, Depends, HTTPException
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from database import MongoDBMotor
+from schemas.responses import ResponseModel
 from services.test_service import TestService
-from schemas.test import TestSchema
+from schemas.test import TestCreateSchema, TestFilterSchema, TestUpdateSchema
 
 router = APIRouter(prefix="/tests", tags=["Tests"])
 
 # Inyectamos el servicio con la DB
-def get_service():
-    return TestService()
+# def get_service():
+#     return TestService()
+
+def get_service( db: AsyncIOMotorDatabase = Depends(MongoDBMotor.get_database)):
+    return TestService(db)
 
 # -------------------------
 # RUTAS CRUD con Beanie
 # -------------------------
-@router.post("/", response_model=dict)
-async def create_test(test: TestSchema, service: TestService = Depends(get_service)):
+@router.post("/", response_model=ResponseModel)
+async def create_test(test: TestCreateSchema, service: TestService = Depends(get_service)):
     test_id = await service.save(test)
-    return {"id": test_id}
+    return ResponseModel(
+        success=True,
+        message="Test creado correctamente",
+        data={"id": test_id},
+        errors=None
+    )
+
+@router.get("/filter")
+async def filter_test(
+    filters: TestFilterSchema = Depends(),
+    service: TestService = Depends(get_service)
+):
+    result = await service.raw_find(filters)
+    return ResponseModel(
+        success=True,
+        message="Tests filtrados correctamente",
+        data= result,
+        errors=None
+    )
 
 
-@router.get("/{test_id}")
+@router.get("/{test_id}", response_model=ResponseModel)
 async def get_test(test_id: str, service: TestService = Depends(get_service)):
     entity = await service.get_by_id(test_id)
     if not entity:
         raise HTTPException(status_code=404, detail="Entity not found")
-    return entity
+    
+    return ResponseModel(
+        success=True,
+        message="Test obtenido correctamente",
+        data= entity,
+        errors=None
+    )
 
 
-# -------------------------
-# RUTAS con consultas raw (Motor)
-# -------------------------
-@router.get("/country/{country}")
-async def find_by_country(country: str, service: TestService = Depends(get_service)):
-    return await service.find_by_country(country)
+@router.get("/", response_model=ResponseModel)
+async def get_all_test( service: TestService = Depends(get_service)):
+    entity = await service.get_all()
+    if not entity:
+        raise HTTPException(status_code=404, detail="Entity not found")
+
+    return ResponseModel(
+        success=True,
+        message="Tests obtenidos correctamente",
+        data= entity,
+        errors=None
+    )
+
+@router.delete("/{test_id}", response_model=ResponseModel)
+async def delete_test(
+    test_id: str,
+    service: TestService = Depends(get_service)
+):
+
+    entity = await service.delete(test_id)
+    if not entity:
+        raise HTTPException(status_code=404, detail="Entity not found")
+
+    return ResponseModel(
+        success=True,
+        message="Tests eliminados correctamente",
+        data= entity,
+        errors=None
+    )
+
+@router.put("/{test_id}", response_model=ResponseModel)
+async def update_test(
+    test_id: str,
+    test: TestUpdateSchema,
+    service: TestService = Depends(get_service)
+):
+    result = await service.update(test_id, test )
+
+    return ResponseModel(
+        success=True,
+        message="Tests Actulizados correctamente",
+        data= result,
+        errors=None
+    )
+
 
 
 @router.get("/aggregate/countries")
