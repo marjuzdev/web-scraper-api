2fe73f5b5fca8581dead8f3974f6a2589d75318f
Mario Juez <marioajuez@gmail.com>
2025-09-15 22:13:20 -0500

    test scrapping with new path

--- src/main.py
+++ src/main.py
@@ -30,7 +30,44 @@ api_router.include_router(test_routes)
 api_router.include_router(seed_routes)
 app.include_router(api_router)
 
+from playwright.async_api import async_playwright
+
 @app.get("/")
 async def root():
     logger.info("Web Scraper API working")
     return {"message": "Web Scraper API working"}
+
+
+@app.get("/quotes")
+async def quotes():
+    url = "https://www.cutis.com.co/hiclina-100-mg-capsulas/p"
+    selectors = {
+        "price_discount": ".vtex-product-price-1-x-sellingPrice .vtex-product-price-1-x-currencyContainer",
+        "price_normal": ".vtex-product-price-1-x-listPriceValue"
+    }
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(
+            headless=True,
+            args=[
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--disable-software-rasterizer"
+            ]
+        )
+        page = await (await browser.new_context()).new_page()
+        await page.goto(url, wait_until="domcontentloaded")
+
+        data = {"url": url}
+        for key, selector in selectors.items():
+            try:
+                el = await page.wait_for_selector(selector, timeout=5000)
+                data[key] = await el.inner_text() if el else None
+            except:
+                data[key] = None
+
+        await browser.close()
+
+    return {"data": data}
